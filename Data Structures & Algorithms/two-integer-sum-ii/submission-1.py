f802ad02d0bcf603e47ad437bb2c4ef57039eac0
Federico Domínguez Molina <67844597+FedericoDM@users.noreply.github.com>
2026-06-07 16:13:12 -0500

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left_pointer = 0
+        right_pointer = len(numbers) - 1
+
+        while left_pointer < len(numbers):
+            if numbers[left_pointer] + numbers[right_pointer] > target:
+                right_pointer += -1
+            elif numbers[left_pointer] + numbers[right_pointer] < target:
+                left_pointer += 1
+            else:
+                # Add one since problem statement says it is a 1-indexed array
+                solution = [left_pointer + 1, right_pointer + 1]
+                return solution
